be55f1b07b427615cf6c8e72267399fe6da21564
melanikoda18 <vmelani193@gmail.com>
2024-04-03 12:07:16 -0400

    lis

---
+++ palindromo/cuenta-letras.py
@@ -0,0 +1,2 @@
+p=len(input("ingrese alguna palabra: "))
+print(f"esta palabra tiene {p} letras ")
---
+++ palindromo/cuenta-palabras.py
@@ -0,0 +1,3 @@
+p=(input("ingrese alguna frase: "))
+c= p.split()
+print(f"esta frase tiene {len (c)} palabras ")
